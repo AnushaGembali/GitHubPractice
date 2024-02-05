00b69258f1b37b1ed10197a2c2e166f85341ec19
AnushaBellala <bellalaanukartik@gmail.com>
2024-02-05 12:57:37 +0530

    second-commit

---
+++ src/main/HomePage.java
@@ -0,0 +1,10 @@
+package main;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+
+		System.out.println("Home Page");
+	}
+
+}
---
+++ src/test/LoginPageTest.java
@@ -0,0 +1,10 @@
+package test;
+
+public class LoginPageTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("Login Page Test");
+	}
+
+}
