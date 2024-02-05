e6195271562e167abadee1871e710c416d36107f
AnushaBellala <bellalaanukartik@gmail.com>
2024-02-05 14:15:24 +0530

    added/removed/modofied Files

--- src/main/LoginPage.java
+++ src/main/LoginPage.java
@@ -5,6 +5,10 @@ public class LoginPage {
 	public static void main(String[] args) {
 
 		System.out.println("Login Page");
+		String username = "admin";
+		String pswrd = "admin";
+		
+		System.out.println("logged in succesfully");
 	}
 
 }
